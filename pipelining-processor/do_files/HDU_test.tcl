82b8981c4b65a117a9fe94c737feeea5268ffa99
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-05-04 16:10:05 +0200

    Add flush1 test cases to testbench

--- pipelining-processor/do_files/HDU_test.tcl
+++ pipelining-processor/do_files/HDU_test.tcl
@@ -25,35 +25,38 @@ run 100
 set insert_bubble [examine -binary sim:/HDU/insert_bubble]
 assert { $insert_bubble == 0 }
 #--------------------------------- END LOAD use test cases ---------------------------------
+#--------------------------------- Flush1 signal test cases --------------------------------
+force -freeze sim:/HDU/swap_F_ID 1 0
+force -freeze sim:/HDU/WB_CU 0 0
+force -freeze sim:/HDU/Rdst1_F_ID 7 0
+force -freeze sim:/HDU/Rdst2_F_ID 5 0
+force -freeze sim:/HDU/Rdst_MEM 7 0
+run 100
+set flush1 [examine -binary sim:/HDU/flush1]
+assert { $flush1 == 1 }
 
-# force -freeze sim:/HDU/Done_Row 0 0
-# force -freeze sim:/HDU/CPU_Bus 02020202 0
-# run 100
-# force -freeze sim:/HDU/CPU_Bus 0a0a0a0a 0
-# run 1200
-# set decompressed [examine -binary sim:/IO_Receive/Memory_Data_Bus]
-# set binary [string range $decompressed 4 67]
-# # test case 1
-# set expected 0011001100000000001111111111000000000011111111110000000000111111‬
-# set expected [string range $expected 0 63]
-# assert { $binary == $expected }
-# run 900
-# # test case 2
-# set decompressed [examine -binary sim:/IO_Receive/Memory_Data_Bus]
-# set binary [string range $decompressed 4 67]
-# set expected 0000000000111111111100000000001111111111000000000011111111110000
-# set expected [string range $expected 0 63]
-# assert { $binary == $expected }
-# # test case 3
-# set decompressed [examine -binary sim:/IO_Receive/Memory_Data_Bus]
-# set binary [string range $decompressed 4 67]
-# set expected 0000000000111111111100000000001111111111000000000011111111110000
-# set expected [string range $expected 0 63]
-# assert { $binary == $expected }
-# run 600
-# # test case 4
-# set decompressed [examine -binary sim:/IO_Receive/Memory_Data_Bus]
-# set binary [string range $decompressed 4 67]
-# set expected 0000001111111111000000000011111111110000000000111111111100000000
-# set expected [string range $expected 0 63]
-# assert { $binary == $expected }
+force -freeze sim:/HDU/WB_CU 1 0
+force -freeze sim:/HDU/swap_F_ID 0 0
+force -freeze sim:/HDU/Rdst_MEM 5 0
+run 100
+set flush1 [examine -binary sim:/HDU/flush1]
+assert { $flush1 == 1 }
+
+force -freeze sim:/HDU/Rdst1_F_ID 5 0
+force -freeze sim:/HDU/swap_F_ID 1 0
+run 100
+set flush1 [examine -binary sim:/HDU/flush1]
+assert { $flush1 == 1 }
+
+force -freeze sim:/HDU/Rdst_MEM 2 0
+run 100
+set flush1 [examine -binary sim:/HDU/flush1]
+assert { $flush1 == 0 }
+
+force -freeze sim:/HDU/swap_F_ID 0 0
+force -freeze sim:/HDU/WB_CU 0 0
+force -freeze sim:/HDU/Rdst_MEM 5 0
+run 100
+set flush1 [examine -binary sim:/HDU/flush1]
+assert { $flush1 == 0 }
+#--------------------------------- END Flush1 signal test cases ------------------------------
