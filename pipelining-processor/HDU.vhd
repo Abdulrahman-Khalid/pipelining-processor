bf420e6b7a1663310ecaf2e25379a9b832e60681
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-05-04 14:23:31 +0200

    Add more HDU inputs

--- pipelining-processor/HDU.vhd
+++ pipelining-processor/HDU.vhd
@@ -7,7 +7,7 @@ entity HDU is
     generic(n: integer := 3);
     port(
         swap_F_ID, swap_ID_E, load_ID_E, load_E_MEM, Branch_MEM: in std_logic;
-        Rdst1_F_ID, Rdst2_F_ID, Rdst1_ID_E, Rdst2_ID_E, Rdst_E_MEM, Rdst_MEM: in std_logic_vector(n-1 downto 0);
+        Rsrc1_F_ID, Rsrc2_F_ID, Rdst1_F_ID, Rdst2_F_ID, Rdst1_ID_E, Rdst2_ID_E, Rdst_E_MEM, Rdst_MEM: in std_logic_vector(n-1 downto 0);
         insert_bubble, flush, hazard_detected: out std_logic;
 end entity;
 
