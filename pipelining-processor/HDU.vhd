106e9126e34fd16dbfbf91cc37869dfebb4dfd32
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2020-05-04 14:39:17 +0200

    add more HDU signals (Write back signals)

--- pipelining-processor/HDU.vhd
+++ pipelining-processor/HDU.vhd
@@ -5,8 +5,10 @@ USE work.constants.all;
 
 entity HDU is
     generic(n: integer := 3);
+    -- WB_F_ID is write back signal in Fetch-Decode buffer 
+    -- WB_ID_E is write back signal in Decode-Execute buffer 
     port(
-        swap_F_ID, swap_ID_E, load_ID_E, load_E_MEM, Branch_MEM: in std_logic;
+        WB_F_ID, WB_ID_E, swap_F_ID, swap_ID_E, load_ID_E, load_E_MEM, Branch_MEM: in std_logic;
         Rsrc1_F_ID, Rsrc2_F_ID, Rdst1_F_ID, Rdst2_F_ID, Rdst1_ID_E, Rdst2_ID_E, Rdst_E_MEM, Rdst_MEM: in std_logic_vector(n-1 downto 0);
         insert_bubble, flush, hazard_detected: out std_logic;
 end entity;
