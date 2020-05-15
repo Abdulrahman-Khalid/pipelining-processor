3fb7d4cd4be949d463f0a1dfc4072a087a589a58
Abdulrahman El Shafei <abdulrahman.khalid@bk.ru>
2020-05-15 10:24:50 +0200

    ready for testing load

--- assembler/test_codes/HDU/ldd_jmps.asm
+++ assembler/test_codes/HDU/ldd_jmps.asm
@@ -16,18 +16,18 @@ rti # interupt logic
 50
 200
 .ORG 10
-LDD r2, 18
-add r2,r3,r1 # r1 -> 18 # stall 1 time r1 -> 30
-jmp r1
-.org 30
-ldd r1, 19 # r1 -> 40
-jmp r1      # stall 3 times
-.org 40
-ldm r7, 60
-jz r7       # r7 -> 60 # no stall
-.org 60
-ldd r2, 20 # r2 -> 50
-jz r2       # r2 -> 140 # no stall
-.org 50
-ldd r2, 21 # r2 -> 200
-call r2       # r2 -> 200 # stall 3 times
+LDD r2, 18 # r2 -> 30                                           # 10, 11
+#add r2,r3,r1 # r1 -> 30 # stall 1 time r1 -> 30                 # 12
+#jmp r1 # jump to address 30                                     # 13
+#.org 30
+#ldd r1, 19 # r1 -> 40
+#jmp r1      # stall 3 times
+#.org 40
+#ldm r7, 60
+#jz r7       # r7 -> 60 # no stall
+#.org 60
+#ldd r2, 20 # r2 -> 50
+#jz r2       # r2 -> 140 # no stall
+#.org 50
+#ldd r2, 21 # r2 -> 200
+#call r2       # r2 -> 200 # stall 3 times
--- pipelining-processor/HDU.vhd
+++ pipelining-processor/HDU.vhd
@@ -21,7 +21,7 @@ entity HDU is
     -- Rdst2_ID_E is Rdst from Decode-Execute buffer
     -- Rdst_E_MEM is Rdst from Execute-Memory buffer
     -- Rdst1_Fetch is Rdst from fetch memory
-    port(WB_CU, WB_ID_E, WB_E_MEM, swap_CU, swap_ID_E, load_ID_E, load_E_MEM, load_MEM_WB, Branch_Fetch, one_src_F_ID, two_src_F_ID: in std_logic;
+    port(WB_CU, WB_ID_E, WB_E_MEM, swap_CU, swap_ID_E, load_ID_E, load_E_MEM, Branch_Fetch, one_src_F_ID, two_src_F_ID: in std_logic;
         Rsrc1_F_ID, Rsrc2_F_ID, Rdst1_F_ID, Rdst2_F_ID, Rdst1_ID_E, Rdst2_ID_E, Rdst_E_MEM, Rdst_MEM_WB, Rdst_Fetch: in std_logic_vector(n-1 downto 0);
         insert_bubble, flush: out std_logic);
 end entity;
@@ -29,15 +29,13 @@ end entity;
 
 
 architecture HDU_Arch of HDU is
-    signal flush1, flush2, flush3: std_logic;
+    signal flush1, flush2: std_logic;
     begin
         flush1 <= '1' when  ( load_ID_E = '1' and Branch_Fetch = '1' and (Rdst1_ID_E = Rdst_Fetch) ) or ( ((swap_CU = '1' and Rdst2_F_ID = Rdst_Fetch) or ((WB_CU = '1' or swap_CU = '1') and Rdst1_F_ID = Rdst_Fetch)) and Branch_Fetch = '1' and load_ID_E = '0' ) else '0';
 
         flush2 <= '1' when ( load_E_MEM = '1' and Branch_Fetch = '1' and (Rdst_E_MEM = Rdst_Fetch) ) or ( ((swap_ID_E = '1' and Rdst2_ID_E = Rdst_Fetch) or ((WB_ID_E = '1' or swap_ID_E = '1') and Rdst1_ID_E = Rdst_Fetch)) and Branch_Fetch = '1' and load_E_MEM = '0' ) else '0';
-        
-        flush3 <= '1' when load_MEM_WB = '1' and Branch_Fetch = '1' and (Rdst_MEM_WB = Rdst_Fetch) else '0';
-        
-        flush <= '1' when flush1 = '1' or flush2 = '1' or flush3 = '1' else '0';
+                
+        flush <= '0' when flush1 = '1' or flush2 = '1' else '0';
 
-        insert_bubble <= '1' when (((Rsrc1_F_ID = Rdst1_ID_E and (one_src_F_ID = '1' or two_src_F_ID = '1')) or (two_src_F_ID = '1' and Rsrc2_F_ID = Rdst1_ID_E)) and load_ID_E = '1') else '0'; -- load use case
+        insert_bubble <= '1' when (((Rsrc1_F_ID = Rdst1_ID_E and (one_src_F_ID = '1' or two_src_F_ID = '1')) or (two_src_F_ID = '1' and Rsrc2_F_ID = Rdst1_ID_E)) and load_E_MEM = '1') else '0'; -- load use case
 end architecture;
