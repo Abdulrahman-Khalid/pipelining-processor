293dd60debc032a546a2f3a19373daec428a31dc
Abdulrahman El Shafei <abdulrahman.khalid@bk.ru>
2020-05-15 11:30:06 +0200

    fix test ldd_jumps code

--- assembler/test_codes/HDU/ldd_jmps.asm
+++ assembler/test_codes/HDU/ldd_jmps.asm
@@ -8,19 +8,25 @@ pop r3
 add r1, r3, r5
 rti # interupt logic
 
-.ORG 0  #this means the the following line would be  at address  0 , and this is the reset address
+.org 0  #this means the the following line would be  at address  0 , and this is the reset address
 10
-.org 18
-30
-40
-50
-200
-.ORG 10
-LDD r2, 18 # r2 -> 30                                           # 10, 11
-#add r2,r3,r1 # r1 -> 30 # stall 1 time r1 -> 30                 # 12
-#jmp r1 # jump to address 30                                     # 13
+
+.org 10
+ldm r2, 30                                                     #10
+std r2, 18                                                     #11, 12
+ldm r2, 40                                                     #13
+std r2, 20                                                     #14, 15
+ldm r2, 50                                                     #16
+std r2, 22                                                     #17, 18
+ldm r2, 200                                                    #19
+std r2, 24                                                     #1A, 1B
+
+LDD r2, 18 # r2 -> 30                                           # 1C, 1D
+
+#add r2,r3,r1 # r1 -> 30 # stall 1 time r1 -> 30                 # 1E
+#jmp r1 # jump to address 30                                     # 1F
 #.org 30
-#ldd r1, 19 # r1 -> 40
+#ldd r1, 18 # r1 -> 40                                          
 #jmp r1      # stall 3 times
 #.org 40
 #ldm r7, 60
@@ -29,5 +35,5 @@ LDD r2, 18 # r2 -> 30                                           # 10, 11
 #ldd r2, 20 # r2 -> 50
 #jz r2       # r2 -> 140 # no stall
 #.org 50
-#ldd r2, 21 # r2 -> 200
+#ldd r2, 22 # r2 -> 200
 #call r2       # r2 -> 200 # stall 3 times
